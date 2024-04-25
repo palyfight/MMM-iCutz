6436e9563019873a7b497154ba255b92eaaf72f7
palyfight <chrisbama92@gmail.com>
2024-04-24 20:07:28 -0400

    fixing file path

--- node_helper.js
+++ node_helper.js
@@ -15,7 +15,7 @@ module.exports = NodeHelper.create({
     },
 
     startCameraScript: function() {
-        const scriptPath = 'script.py';
+        const scriptPath = 'MMM-iCutz/script.py';
         this.pyShell = spawn('python3', [scriptPath]);
         this.pyShell.stdout.on('data', (data) => {
             console.log("Python script output:", data.toString());

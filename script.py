6c79d72ca67ea3dafac970920bb58032ae37f5c8
palyfight <chrisbama92@gmail.com>
2024-04-30 19:23:31 -0400

    removing cv2 from the module. Too slow to install opencv

--- script.py
+++ script.py
@@ -23,21 +23,23 @@ try:
     while True:
         timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         image_path = f'modules/MMM-iCutz/public/{timestamp}.jpg'
-        camera.capture("temporary.jpg")  # Capture image
+        camera.capture(image_path)
+        
+        # camera.capture("temporary.jpg")  # Capture image
 
-        # Read the image and convert to grayscale for face detection
-        img = cv2.imread("temporary.jpg")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # # Read the image and convert to grayscale for face detection
+        # img = cv2.imread("temporary.jpg")
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces in the image
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        # # Detect faces in the image
+        # faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-        # Draw rectangles around each face
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # # Draw rectangles around each face
+        # for (x, y, w, h) in faces:
+        #     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-        # Save the modified image
-        cv2.imwrite(image_path, img)
+        # # Save the modified image
+        # cv2.imwrite(image_path, img)
 
         # Clean up and sleep
         cleanup_old_images(10)  # Adjust the number based on your preference

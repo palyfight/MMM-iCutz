8bce09013ecb9000e8690c39e7d16494db24b9f3
palyfight <chrisbama92@gmail.com>
2024-05-02 20:33:17 -0400

    it works!

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from picamera import PiCamera
 from time import sleep
 import datetime
 
-def cleanup_old_images(max_images=10):
+def cleanup_old_images(max_images=50):
     """Keep only the latest max_images files."""
     files = sorted(glob.glob('modules/MMM-iCutz/public/*.jpg'))
     if len(files) > max_images:
@@ -21,8 +21,8 @@ try:
         image_path = f'modules/MMM-iCutz/public/{timestamp}.jpg'
         camera.capture(image_path)
 
-        print(image_path)
-        
+        print(image_path, flush=True)  # Ensure the output is flushed immediately
+
         # Clean up and sleep
         cleanup_old_images(10)  # Adjust the number based on your preference
         sleep(5)  # Update interval
